f2ad468785e443f5b5764c70d4375f2f520660f9
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-20 02:51:50 +0530

    [Shivaraj] Add the gender column and add details

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -25,3 +25,8 @@ select * from employee_payroll;
 --UC5-- Retrieve salary of particular employee and particular date range
 select salary from employee_payroll where Name = 'Virat';
 select * from employee_payroll where StartDate between cast ('2018-01-01' as date) and GETDATE();
+
+--UC6-- add Gender to Employee_Payroll Table and Update the Rows to reflect the correct Employee Gender
+alter table employee_payroll add Gender char(1);
+update employee_payroll set Gender = 'M' where Id in (1,2,3);
+update employee_payroll set Gender = 'F' where Id in (4,5);
